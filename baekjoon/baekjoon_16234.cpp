a6e0e043ba6bfc43c1e8f5028f70836f643b38c2
Subin Jeon <ideun_97@naver.com>
2020-05-18 16:37:38 +0900

    update #16234

--- baekjoon/baekjoon_16234.cpp
+++ baekjoon/baekjoon_16234.cpp
@@ -1,102 +1,98 @@
 #include<iostream>
-#include<unordered_map>
 #include<utility>
+#include<tuple>
+#include<cmath>
+#include<cstring>
 using namespace std;
 
-typedef struct Peo_Nat{
-    int people;
-    int nations;
-} peo_nat;
-
 int N, L, R;
-int map[50][50];
-int unite[50][50] = {0};
-unordered_map<int, peo_nat> unite_map;
+int A[50][50];	// 인구
+int B[50][50];	// 연합 번호
+int C[2500];	// 연합 번호 별 각 나라의 새로운 인구
 
-bool valid_idx(int x, int y)
+bool valid_unite(int x, int y, int temp_x, int temp_y)
 {
-    if(0 <= x && x < N && 0 <= y && y < N)
-        return true;
-    return false;
+	if(0 <= temp_x && temp_x < N && 0 <= temp_y && temp_y < N){
+		if(B[temp_x][temp_y] == -1){
+			int diff = abs(A[x][y] - A[temp_x][temp_y]);
+			if(L <= diff && diff <= R){
+				return true;
+			}
+			return false;
+		}
+		return false;
+	}
+	return false;
 }
 
-void DFS(int x, int y, int unite_num, int& people, int& nations)
+pair<int, int> make_unite(int x, int y, int num)
 {
-    unite[x][y] = unite_num; // 해당 연합의 고유 번호
-    people += map[x][y]; // 해당 연합에 포함된 나라의 인구수 합
-    nations++; // 해당 연합에 포함된 나라 수
-
-    int dx[] = {0, 0, -1, 1};
-    int dy[] = {-1, 1, 0, 0};
-
-    int temp_x, temp_y, diff;
-    for(int i=0; i<4; i++){
-        temp_x = x + dx[i];
-        temp_y = y + dy[i];
-
-        if(valid_idx(temp_x, temp_y) && !unite[temp_x][temp_y]){
-            diff = abs(map[x][y] - map[temp_x][temp_y]);
-            if(L <= diff && diff <= R){
-                DFS(temp_x, temp_y, unite_num, people, nations);
-            }
-        }
-    }
-}
-
-void move(int unite_num, int people, int nations)
-{
-    int distributed_people = people / nations;
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N; j++){
-            if(unite[i][j] == unite_num){
-                map[i][j] = distributed_people;
-            }
-        }
-    }
+	// x, y: 연합을 이룰 시작점 좌표 (B[x][y] == -1) 
+	// num: 연합 번호
+	
+	B[x][y] = num;
+	int people = A[x][y], nation = 1;
+	
+	int dx[] = {0, 0, -1, 1};
+	int dy[] = {-1, 1, 0, 0};
+	
+	int temp_x, temp_y;
+	for(int i=0; i<4; i++){
+		temp_x = x + dx[i];
+		temp_y = y + dy[i];
+		
+		if(valid_unite(x, y, temp_x, temp_y)){
+			int add_people, add_nation;
+			tie(add_people, add_nation) = make_unite(temp_x, temp_y, num);
+			people += add_people;
+			nation += add_nation;
+		}
+	}
+	
+	return make_pair(people, nation);
 }
 
 int main()
 {
-    cin >> N >> L >> R;
-    for(int i=0; i<N; i++)
-        for(int j=0; j<N; j++)  cin >> map[i][j];
-
-    int unite_num = 1, people = 0, nations = 0, result = 0;
-    bool move_flag = false;
-    peo_nat pn;
-
-    while(true){
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                if(!unite[i][j]){
-                    DFS(i, j, unite_num, people, nations);
-                    pn.people = people;
-                    pn.nations = nations;
-                    unite_map.insert(make_pair(unite_num, pn));
-
-                    unite_num++;
-                    people = 0;
-                    nations = 0;
-                }
-            }
-        }
-
-        for(int i=0; i<unite_num; i++){
-            people = (unite_map.find(i) -> second).people;
-            nations = (unite_map.find(i) -> second).nations;
-
-            if(nations >= 2){
-                move(i, people, nations);
-                move_flag = true;
-            }
-        }
-
-        if(move_flag)   result++;
-        else    break;
-
-        move_flag = false;
-        unite_map.clear();
-    }
-
-    cout << result;
+	cin >> N >> L >> R;
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			cin >> A[i][j];
+		}
+	}
+	
+	int move = 0; // 인구 이동 횟수
+	bool move_flag = false;
+	int people, nation;
+	
+	while(true){
+		int unite_num = 0;
+		memset(B, -1, sizeof(B));
+		memset(C, 0, sizeof(C));
+		move_flag = false;
+		
+		for(int i=0; i<N; i++){
+			for(int j=0; j<N; j++){
+				if(B[i][j] == -1){ // 연합을 이룰 시작점 (아직 연합이 없음)
+					tie(people, nation) = make_unite(i, j, unite_num);
+					if(nation > 1)	move_flag = true;
+					
+					C[unite_num] = people / nation;
+					unite_num++;
+				}
+			}
+		}
+		
+		if(move_flag){
+			move++;
+			for(int i=0; i<N; i++){
+				for(int j=0; j<N; j++){
+					A[i][j] = C[B[i][j]]; // B[i][j]가 unite_num에 해당
+				}
+			}
+		}
+		else	break;
+	}
+	
+	cout << move;
 }
